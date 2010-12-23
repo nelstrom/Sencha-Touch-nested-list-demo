be26731e71f9bb0133d1eaf0e51fa6f193ec2214
Drew Neil <andrew.jr.neil@gmail.com>
2010-12-23 17:39:52 +0200

    Switch from List to Nested List.

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,72 +1,72 @@
-Ext.regModel('Contact', {
-    fields: ['firstName', 'lastName']
+// store with data
+demos.data = {
+    text: 'Groceries',
+    items: [{
+        text: 'Drinks',
+        items: [{
+            text: 'Water',
+            items: [{
+                text: 'Sparkling',
+                leaf: true
+            },{
+                text: 'Still',
+                leaf: true
+            }]
+        },{
+            text: 'Coffee',
+            leaf: true
+        },{
+            text: 'Espresso',
+            leaf: true
+        },{
+            text: 'Redbull',
+            leaf: true
+        },{
+            text: 'Coke',
+            leaf: true
+        },{
+            text: 'Diet Coke',
+            leaf: true
+        }]
+    },{
+        text: 'Fruit',
+        items: [{
+            text: 'Bananas',
+            leaf: true
+        },{
+            text: 'Lemon',
+            leaf: true
+        }]
+    },{
+        text: 'Snacks',
+        items: [{
+            text: 'Nuts',
+            leaf: true
+        },{
+            text: 'Pretzels',
+            leaf: true
+        },{
+            text: 'Wasabi Peas',
+            leaf: true
+        }]
+    },{
+        text: 'Empty Category',
+        items: []
+    }]
+};
+
+Ext.regModel('ListItem', {
+    fields: [{name: 'text', type: 'string'}]
 });
 
-demos.ListStore = new Ext.data.Store({
-    model: 'Contact',
-    sorters: 'lastName',
-    getGroupString : function(record) {
-        return record.get('lastName')[0];
-    },
-    data: [
-        { firstName: 'Julio',      lastName: 'Benesh'},
-        { firstName: 'Julio',      lastName: 'Minich'},
-        { firstName: 'Tania',      lastName: 'Ricco'},
-        { firstName: 'Odessa',     lastName: 'Steuck'},
-        { firstName: 'Nelson',     lastName: 'Raber'},
-        { firstName: 'Tyrone',     lastName: 'Scannell'},
-        { firstName: 'Allan',      lastName: 'Disbrow'},
-        { firstName: 'Cody',       lastName: 'Herrell'},
-        { firstName: 'Julio',      lastName: 'Burgoyne'},
-        { firstName: 'Jessie',     lastName: 'Boedeker'},
-        { firstName: 'Allan',      lastName: 'Leyendecker'},
-        { firstName: 'Javier',     lastName: 'Lockley'},
-        { firstName: 'Guy',        lastName: 'Reasor'},
-        { firstName: 'Jamie',      lastName: 'Brummer'},
-        { firstName: 'Jessie',     lastName: 'Casa'},
-        { firstName: 'Marcie',     lastName: 'Ricca'},
-        { firstName: 'Gay',        lastName: 'Lamoureaux'},
-        { firstName: 'Althea',     lastName: 'Sturtz'},
-        { firstName: 'Kenya',      lastName: 'Morocco'},
-        { firstName: 'Rae',        lastName: 'Pasquariello'},
-        { firstName: 'Ted',        lastName: 'Abundis'},
-        { firstName: 'Jessie',     lastName: 'Schacherer'},
-        { firstName: 'Jamie',      lastName: 'Gleaves'},
-        { firstName: 'Hillary',    lastName: 'Spiva'},
-        { firstName: 'Elinor',     lastName: 'Rockefeller'},
-        { firstName: 'Dona',       lastName: 'Clauss'},
-        { firstName: 'Ashlee',     lastName: 'Kennerly'},
-        { firstName: 'Alana',      lastName: 'Wiersma'},
-        { firstName: 'Kelly',      lastName: 'Holdman'},
-        { firstName: 'Mathew',     lastName: 'Lofthouse'},
-        { firstName: 'Dona',       lastName: 'Tatman'},
-        { firstName: 'Clayton',    lastName: 'Clear'},
-        { firstName: 'Rosalinda',  lastName: 'Urman'},
-        { firstName: 'Cody',       lastName: 'Sayler'},
-        { firstName: 'Odessa',     lastName: 'Averitt'},
-        { firstName: 'Ted',        lastName: 'Poage'},
-        { firstName: 'Penelope',   lastName: 'Gayer'},
-        { firstName: 'Katy',       lastName: 'Bluford'},
-        { firstName: 'Kelly',      lastName: 'Mchargue'},
-        { firstName: 'Kathrine',   lastName: 'Gustavson'},
-        { firstName: 'Kelly',      lastName: 'Hartson'},
-        { firstName: 'Carlene',    lastName: 'Summitt'},
-        { firstName: 'Kathrine',   lastName: 'Vrabel'},
-        { firstName: 'Roxie',      lastName: 'Mcconn'},
-        { firstName: 'Margery',    lastName: 'Pullman'},
-        { firstName: 'Avis',       lastName: 'Bueche'},
-        { firstName: 'Esmeralda',  lastName: 'Katzer'},
-        { firstName: 'Tania',      lastName: 'Belmonte'},
-        { firstName: 'Malinda',    lastName: 'Kwak'},
-        { firstName: 'Tanisha',    lastName: 'Jobin'},
-        { firstName: 'Kelly',      lastName: 'Dziedzic'},
-        { firstName: 'Darren',     lastName: 'Devalle'},
-        { firstName: 'Julio',      lastName: 'Buchannon'},
-        { firstName: 'Darren',     lastName: 'Schreier'},
-        { firstName: 'Jamie',      lastName: 'Pollman'},
-        { firstName: 'Karina',     lastName: 'Pompey'},
-        { firstName: 'Hugh',       lastName: 'Snover'},
-        { firstName: 'Zebra',      lastName: 'Evilias'}
-    ]
+demos.store = new Ext.data.TreeStore({
+    model: 'ListItem',
+    root: demos.data,
+    proxy: {
+        type: 'ajax',
+        reader: {
+            type: 'tree',
+            root: 'items'
+        }
+    }
 });
-
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,44 +1,11 @@
-Ext.ns('sink', 'demos', 'Ext.ux');
+Ext.ns('demos');
 Ext.setup({
     onReady: function() {
-
-        demos.simpleList = new Ext.List({
-            xtype: 'list',
-            store: demos.ListStore,
-            itemTpl: '<div class="contact">{firstName} {lastName}</div>'
-        });
-
-        demos.groupedList = new Ext.List({
-            store: demos.ListStore,
-            itemTpl: '<div class="contact">{firstName} {lastName}</div>',
-            grouped: true,
-            indexBar: true
-        });
-
-        demos.disclosureList = new Ext.List({
-            onItemDisclosure: function(record, btn, index) {
-                Ext.Msg.alert('Tap', 'Disclose more info for ' + record.get('firstName'), Ext.emptyFn);
-            },
-            store: demos.ListStore,
-            itemTpl: '<div class="contact">{firstName} {lastName}</div>'
-        });
-
-        demos.List = new Ext.TabPanel ({
+        var nestedList = new Ext.NestedList({
             fullscreen: true,
-            items: [{
-                title: 'Simple',
-                items: [demos.simpleList]
-            }, {
-                title: 'Grouped',
-                items: [demos.groupedList]
-            }, {
-                title: 'Disclosure',
-                items: [demos.disclosureList]
-            }],
-            defaults: {
-                layout: 'fit',
-                cls: 'demo-list',
-            }
+            title: 'Groceries',
+            displayField: 'text',
+            store: demos.store
         });
         
         
