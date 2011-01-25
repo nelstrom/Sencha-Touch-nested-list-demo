871b7c76da72bd66b8fa613bd37094a373b681c2
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-25 18:23:57 +0200

    Read data as JSON request, rather than inline.

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,60 +1,3 @@
-// store with data
-NestedListDemo.data = {
-    text: 'Groceries',
-    items: [{
-        text: 'Drinks',
-        items: [{
-            text: 'Water',
-            items: [{
-                text: 'Sparkling',
-                leaf: true
-            },{
-                text: 'Still',
-                leaf: true
-            }]
-        },{
-            text: 'Coffee',
-            leaf: true
-        },{
-            text: 'Espresso',
-            leaf: true
-        },{
-            text: 'Redbull',
-            leaf: true
-        },{
-            text: 'Coke',
-            leaf: true
-        },{
-            text: 'Diet Coke',
-            leaf: true
-        }]
-    },{
-        text: 'Fruit',
-        items: [{
-            text: 'Bananas',
-            leaf: true
-        },{
-            text: 'Lemon',
-            leaf: true
-        }]
-    },{
-        text: 'Snacks',
-        items: [{
-            text: 'Nuts',
-            leaf: true
-        },{
-            text: 'Pretzels',
-            leaf: true
-        },{
-            text: 'Wasabi Peas',
-            leaf: true
-        }]
-    },{
-        text: 'Empty Category',
-        items: []
-    }]
-};
-
 Ext.regModel('ListItem', {
     fields: [{name: 'text', type: 'string'}]
 });
@@ -64,6 +7,7 @@ NestedListDemo.store = new Ext.data.TreeStore({
     root: NestedListDemo.data,
     proxy: {
         type: 'ajax',
+        url: '/groceries.json',
         reader: {
             type: 'tree',
             root: 'items'
