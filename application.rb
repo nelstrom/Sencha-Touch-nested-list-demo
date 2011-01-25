bff03a7812713e366903bf9540fdec1fc0f4ffe5
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-25 18:16:31 +0200

    Add a /groceries.json path to Sinatra app.

--- application.rb
+++ application.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
+mime_type :json, 'application/json'
+
 get '/' do
   erb :index
+end
+
+get '/groceries.json' do
+  send_file 'views/groceries.json', :type => :json
 end
