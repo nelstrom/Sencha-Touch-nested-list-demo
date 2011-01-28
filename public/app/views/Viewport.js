395676201ea1d3dc5ac14dc735b353616fafc56c
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-28 11:03:08 +0200

    Add a tab with NestedList for tracks.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -5,12 +5,14 @@ NestedListDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
     initComponent: function() {
         //put instances of cards into NestedListDemo.views namespace
         Ext.apply(NestedListDemo.views, {
-            albumsList: new NestedListDemo.views.Albums()
+            albumsList: new NestedListDemo.views.Albums(),
+            tracksList: new NestedListDemo.views.Tracks(),
         });
         //put instances of cards into viewport
         Ext.apply(this, {
             items: [
-                NestedListDemo.views.albumsList
+                NestedListDemo.views.albumsList,
+                NestedListDemo.views.tracksList
             ]
         });
         NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
---
+++ public/app/views/tracks.js
@@ -0,0 +1,16 @@
+NestedListDemo.views.Tracks = Ext.extend(Ext.NestedList, {
+    title: 'Tracks',
+    displayField: 'text',
+    store: NestedListDemo.tracks_store,
+    // getDetailCard: function(item, parent) {
+    //     var itemData = item.attributes.record.data,
+    //     parentData = parent.attributes.record.data,
+    //     detailCard = new Ext.Panel({
+    //         tpl: "detail card for {text}"
+    //     });
+    //     this.backButton.setText(parentData.text);
+    //     this.toolbar.setTitle(itemData.text);
+    //     detailCard.update(itemData);
+    //     return detailCard;
+    // }
+});
