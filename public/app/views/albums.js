063ff2f01266e045ce5b6cc23fd62084557885cc
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-28 11:09:24 +0200

    Enable detail panel for albums/tracks.

--- public/app/views/albums.js
+++ public/app/views/albums.js
@@ -2,15 +2,15 @@ NestedListDemo.views.Albums = Ext.extend(Ext.NestedList, {
     title: 'Albums',
     displayField: 'text',
     store: NestedListDemo.albums_store,
-    // getDetailCard: function(item, parent) {
-    //     var itemData = item.attributes.record.data,
-    //     parentData = parent.attributes.record.data,
-    //     detailCard = new Ext.Panel({
-    //         tpl: "detail card for {text}"
-    //     });
-    //     this.backButton.setText(parentData.text);
-    //     this.toolbar.setTitle(itemData.text);
-    //     detailCard.update(itemData);
-    //     return detailCard;
-    // }
+    getDetailCard: function(item, parent) {
+        var itemData = item.attributes.record.data,
+        parentData = parent.attributes.record.data,
+        detailCard = new Ext.Panel({
+            tpl: "detail card for {text}"
+        });
+        this.backButton.setText(parentData.text);
+        this.toolbar.setTitle(itemData.text);
+        detailCard.update(itemData);
+        return detailCard;
+    }
 });
