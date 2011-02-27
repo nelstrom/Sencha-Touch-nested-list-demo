4189a209a86c0e07ac52e4cf96d2824cfe883b10
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-27 12:13:09 +0200

    Wipe out all functionality to revert to blank slate.

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,5 @@
 NestedListDemo = new Ext.Application({
     name: "NestedListDemo",
 
-    launch: function() {
-        this.views.viewport = new this.views.Viewport();
-    }
+    launch: function() {}
 });
--- public/app/models/list_item.js
+++
@@ -1,9 +0,0 @@
-Ext.regModel('ListItem', {
-    fields: [
-		{name: 'text', type: 'string'},
-		{name: 'info', type: 'string'},
-		{name: 'model', type: 'string'},
-		{name: 'items', type: 'auto'},
-		{name: 'duration', type: 'auto'},
-	]
-});
