f93fb1b1453fe3ee438eca3bcbb5bdc8e5f366b6
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-27 12:44:04 +0200

    Create panel for leaf nodes using getDetailCard.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -5,6 +5,19 @@ NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
             title: 'Music',
             displayField: 'text',
             store: NestedListDemo.music_store,
+            getDetailCard: function(item, parent) {
+                var itemData = item.attributes.record.data,
+                parentData = parent.attributes.record.data,
+                detailCard = new Ext.Panel({
+                    scroll: 'vertical',
+                    styleHtmlContent: true,
+                    tpl: ["<h2>{text}</h2>","{info}"]
+                });
+                this.backButton.setText(parentData.text);
+                this.toolbar.setTitle(itemData.text);
+                detailCard.update(itemData);
+                return detailCard;
+            }
         });
         NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
     }
