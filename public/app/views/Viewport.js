32615e6812e6c8def3011dd8729814a9001b7b5d
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-28 09:54:58 +0200

    Leave displayField undefined. (default is 'text' anyway)

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -3,7 +3,6 @@ NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
         Ext.apply(this, {
             fullscreen: true,
             title: 'Music',
-            displayField: 'text',
             store: NestedListDemo.music_store,
         });
         NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
