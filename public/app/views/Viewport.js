ba8cb379c7b89549658709aa932cf028a9d60ade
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-28 10:31:32 +0200

    Simplify NestedList config by not using Ext.apply()

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,23 +1,18 @@
 NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
-    initComponent: function() {
-        Ext.apply(this, {
-            fullscreen: true,
-            title: 'Music',
-            displayField: 'text',
-            store: NestedListDemo.music_store,
-            getDetailCard: function(item, parent) {
-                var itemData = item.attributes.record.data,
-                parentData = parent.attributes.record.data,
-                detailCard = new Ext.Panel({
-                    scroll: 'vertical',
-                    styleHtmlContent: true,
-                    tpl: ["<h2>{text}</h2>","{info}"]
-                });
-                detailCard.update(itemData);
-                this.backButton.setText(parentData.text);
-                return detailCard;
-            }
+    fullscreen: true,
+    title: 'Music',
+    displayField: 'text',
+    store: NestedListDemo.music_store,
+    getDetailCard: function(item, parent) {
+        var itemData = item.attributes.record.data,
+        parentData = parent.attributes.record.data,
+        detailCard = new Ext.Panel({
+            scroll: 'vertical',
+            styleHtmlContent: true,
+            tpl: ["<h2>{text}</h2>","{info}"]
         });
-        NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
+        detailCard.update(itemData);
+        this.backButton.setText(parentData.text);
+        return detailCard;
     }
 });
