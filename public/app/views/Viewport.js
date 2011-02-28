4eb245c6bd115a186e29db4eb9d4c72386cc4361
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-28 10:32:26 +0200

    Simplify NestedList config by not using Ext.apply()

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,42 +1,37 @@
 NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
-    initComponent: function() {
-        Ext.apply(this, {
-            fullscreen: true,
-            title: 'Music',
-            displayField: 'text',
-            store: NestedListDemo.music_store,
-            getDetailCard: function(item, parent) {
-                var itemData = item.attributes.record.data,
-                parentData = parent.attributes.record.data,
-                detailCard = new Ext.Panel({
-                    scroll: 'vertical',
-                    styleHtmlContent: true,
-                    tpl: ["<h2>{text}</h2>","{info}"]
-                });
-                detailCard.update(itemData);
-                this.backButton.setText(parentData.text);
-                return detailCard;
-            },
-            getItemTextTpl: function() {
-                var tplConstructor = '{text}' +
-                    '<tpl if="model === \'Artist\'">'+
-                        '<div class="metadata">' +
-                            ' {[values.items.length]} albums' +
-                        '</div>' +
-                    '</tpl>' +
-                    '<tpl if="model === \'Album\'">'+
-                        '<div class="metadata">' +
-                            ' {[values.items.length]} tracks' +
-                        '</div>' +
-                    '</tpl>' +
-                    '<tpl if="model === \'Track\'">'+
-                        '<div class="metadata">' +
-                            ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
-                        '</div>' +
-                    '</tpl>';
-                return tplConstructor;
-            }
+    fullscreen: true,
+    title: 'Music',
+    displayField: 'text',
+    store: NestedListDemo.music_store,
+    getDetailCard: function(item, parent) {
+        var itemData = item.attributes.record.data,
+        parentData = parent.attributes.record.data,
+        detailCard = new Ext.Panel({
+            scroll: 'vertical',
+            styleHtmlContent: true,
+            tpl: ["<h2>{text}</h2>","{info}"]
         });
-        NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
+        detailCard.update(itemData);
+        this.backButton.setText(parentData.text);
+        return detailCard;
+    },
+    getItemTextTpl: function() {
+        var tplConstructor = '{text}' +
+            '<tpl if="model === \'Artist\'">'+
+                '<div class="metadata">' +
+                    ' {[values.items.length]} albums' +
+                '</div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Album\'">'+
+                '<div class="metadata">' +
+                    ' {[values.items.length]} tracks' +
+                '</div>' +
+            '</tpl>' +
+            '<tpl if="model === \'Track\'">'+
+                '<div class="metadata">' +
+                    ' Duration: {[Math.floor(values.duration/60)]}:{[values.duration%60]}' +
+                '</div>' +
+            '</tpl>';
+        return tplConstructor;
     }
 });
