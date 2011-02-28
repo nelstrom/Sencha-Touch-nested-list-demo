a4dbff3accfbe145f68625d794091fa78f93890f
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-28 10:30:24 +0200

    Simplify NestedList config, by not using Ext.apply()

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,10 +1,5 @@
 NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
-    initComponent: function() {
-        Ext.apply(this, {
-            fullscreen: true,
-            title: 'Music',
-            store: NestedListDemo.music_store,
-        });
-        NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
-    }
+    fullscreen: true,
+    title: 'Music',
+    store: NestedListDemo.music_store,
 });
