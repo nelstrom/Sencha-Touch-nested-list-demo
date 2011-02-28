d466e9437c0d835152722ef050a15c2df87a0166
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-28 10:36:24 +0200

    Make the blank_slate slightly less blank.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -1,22 +1,4 @@
-NestedListDemo.views.Viewport = Ext.extend(Ext.TabPanel, {
+NestedListDemo.views.Viewport = Ext.extend(Ext.Panel, {
     fullscreen: true,
-    layout: 'card',
-    cardSwitchAnimation: 'flip',
-    initComponent: function() {
-        //put instances of cards into NestedListDemo.views namespace
-        Ext.apply(NestedListDemo.views, {
-            artistsList: new NestedListDemo.views.Artists(),
-            albumsList: new NestedListDemo.views.Albums(),
-            tracksList: new NestedListDemo.views.Tracks(),
-        });
-        //put instances of cards into viewport
-        Ext.apply(this, {
-            items: [
-                NestedListDemo.views.tracksList,
-                NestedListDemo.views.albumsList,
-                NestedListDemo.views.artistsList
-            ]
-        });
-        NestedListDemo.views.Viewport.superclass.initComponent.apply(this, arguments);
-    }
+    html: 'It works!',
 });
