a3286fa736dd8fb1b34ff14bbd7e71f52ea34cad
Drew Neil <andrew.jr.neil@gmail.com>
2011-03-01 11:59:32 +0200

    Create a single detail panel, and reuse it.

--- public/app/views/Viewport.js
+++ public/app/views/Viewport.js
@@ -5,15 +5,10 @@ NestedListDemo.views.Viewport = Ext.extend(Ext.NestedList, {
     store: NestedListDemo.music_store,
     getDetailCard: function(item, parent) {
         var itemData = item.attributes.record.data,
-        parentData = parent.attributes.record.data,
-        detailCard = new Ext.Panel({
-            scroll: 'vertical',
-            styleHtmlContent: true,
-            tpl: ["<h2>{text}</h2>","{info}"]
-        });
-        detailCard.update(itemData);
+        parentData = parent.attributes.record.data;
+        NestedListDemo.views.detailCard.update(itemData);
         this.backButton.setText(parentData.text);
-        return detailCard;
+        return NestedListDemo.views.detailCard;
     },
     getItemTextTpl: function() {
         var tplConstructor = '{text}' +
